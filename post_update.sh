9e3e09cbd7f8673f0d02c7889b484294f7b34d6b
Yves <yves.dornier@yahoo.fr>
2021-09-19 18:32:32 +0200

    Fix post_update.sh

--- post_update.sh
+++ post_update.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-#Just for test
-ps h -p $$ -o args='' | cut -f1 -d' '
-
 package_version_url="http://downloads.slimdevices.com/nightly/?ver=8.1"
 service_name="logitechmediaserver"
 work_dir="/var/lib/${service_name}"
@@ -14,6 +11,9 @@ logitechmediaserver_group=squeezebox
 
 chmod +x "/usr/local/etc/rc.d/${service_name}"
 service "${service_name}" start
+
+sleep 5
+curl -s http://127.0.0.1:9000 > /dev/null
 if [ $? -ne 0 ]; then
     # Find the perl version x.y or x.yy
     perl_major_version=$(perl -e 'print "$^V\n"' | cut -c 2- | cut -d "." -f 1)
@@ -26,7 +26,15 @@ if [ $? -ne 0 ]; then
         echo "No CPAN Binary matching to your Perl version."
         exit 1
     fi
-    service "${service_name}" start
+
+    pgrep -q perl
+    if [ $? -ne 0 ]; then
+        service "${service_name}" start
+    else 
+        service "${service_name}" restart
+
+    sleep 5
+    curl -s http://127.0.0.1:9000 > /dev/null
     if [ $? -ne 0 ]; then
         exit 1
     fi
